2ebb4e50450e87eaeb18854fb8468d6eae832444
Atsukin <atsuki.tamura.2740@gmail.com>
2021-08-09 19:18:07 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './homepage.component'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
