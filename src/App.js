f4084b5c718bad2a2d243d94dec30b8186d947d1
Atsukin <atsuki.tamura.2740@gmail.com>
2021-08-10 10:20:27 +0900

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import {Route, Link} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+
+
+const TopicsList = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Topic List</h1>
+        </div>
+    )
+}
+const TopicDetail = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Topic Detail Page: {props.match.params.topicId}</h1>
+        </div>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
       <div>
-        <HomePage />
+        <Route path='/' component={HomePage} />
+        <Route exact path='/topics' component={TopicsList} />
+        <Route path='/topics/:topicId' component={TopicDetail} />
       </div>
-    </div>
   );
 }
 
